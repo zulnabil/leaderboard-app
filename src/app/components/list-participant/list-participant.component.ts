f9c5cf8f1af9ef7b97829b6546c04c8d6d67ee51
Zulnabil Alhadad <zulnabil.22@gmail.com>
2024-07-29 17:20:06 +0700

    feat(participants): implement user detail when click a participant

--- src/app/components/list-participant/list-participant.component.ts
+++ src/app/components/list-participant/list-participant.component.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { UserDetailsComponent } from '@app/components/user-details/user-details.component';
 import { AvatarUrlPipe } from '@app/pipes/avatar-url.pipe';
 import { RandomPointsPipe } from '@app/pipes/random-points.pipe';
 import { UserService } from '@app/services/user.service';
+import { ModalComponent } from '@app/shared/modal/modal.component';
 import { User } from '@app/types/feature';
 
 @Component({
   selector: 'app-list-participant',
   standalone: true,
-  imports: [AvatarUrlPipe, RandomPointsPipe],
+  imports: [
+    AvatarUrlPipe,
+    RandomPointsPipe,
+    ModalComponent,
+    UserDetailsComponent,
+  ],
   templateUrl: './list-participant.component.html',
   styleUrl: './list-participant.component.scss',
 })
 export class ListParticipantComponent implements OnInit {
   participants: User[] = [];
+  selectedParticipant: number | undefined;
+  isDetailParticipantOpen = false;
 
   constructor(private userService: UserService) {}
 
@@ -21,5 +30,15 @@ export class ListParticipantComponent implements OnInit {
       const excludeFirstThree = data.slice(3); // Exclude the first three items
       this.participants = excludeFirstThree;
     });
+  }
+
+  handleSelectUser(id: number) {
+    this.selectedParticipant = id;
+    this.isDetailParticipantOpen = true;
+  }
+
+  handleCloseDetail() {
+    this.isDetailParticipantOpen = false;
+    this.selectedParticipant = undefined;
   }
 }
