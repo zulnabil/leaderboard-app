b74befb2329cf329894613ecc37178adecb3d377
Zulnabil Alhadad <zulnabil.22@gmail.com>
2024-07-29 14:37:00 +0700

    feat(participant): slice UI participant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ContainerComponent } from '@app/core/layout/container/container.component';
-import { HeaderComponent } from '@app/core/layout/header/header.component';
+import { ListParticipantComponent } from '@app/list-participant/list-participant.component';
 import { WinnersComponent } from '@app/winners/winners.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ContainerComponent, WinnersComponent],
+  imports: [
+    RouterOutlet,
+    ContainerComponent,
+    WinnersComponent,
+    ListParticipantComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
---
+++ src/app/list-participant/list-participant.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-participant',
+  standalone: true,
+  imports: [],
+  templateUrl: './list-participant.component.html',
+  styleUrl: './list-participant.component.scss'
+})
+export class ListParticipantComponent {
+
+}
